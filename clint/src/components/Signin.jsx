9f9cfedd64ff187570ba08c56e06f44e66238f92
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2022-12-09 01:31:17 +0530

    entered

--- clint/src/components/Signin.jsx
+++ clint/src/components/Signin.jsx
@@ -2,21 +2,36 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import moment from "moment";
+import date from "date-and-time";
 
 function Signin() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [logout, setLogout] = useState(false);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const parsedTime = localStorage.getItem("TIME");
-    const nowTime = new moment().toString();
-    var diffTime =
-      moment(parsedTime).add(1, "days") - moment(nowTime).add(0, "minutes");
-    // console.log("difference",diffTime)
-    if (diffTime >= 0) {
+    let hour = localStorage.getItem("hour");
+    let minute = localStorage.getItem("minute");
+    let second = localStorage.getItem("second");
+    let year = localStorage.getItem("year");
+    let month = localStorage.getItem("month");
+    let date2 = localStorage.getItem("date2");
+    const now = new Date();
+    const date1 = new Date(year, month, date2, hour, minute, second);
+    date1.setHours(date1.getHours() + 23);
+    date.format(date1, "YYYY/MM/DD HH:mm:ss");
+    console.log(year, month, date2, hour, minute, second, "pintu");
+    const value = date.subtract(date1, now);
+
+    console.log("total days between them " + value.toHours());
+    let x = value.toHours();
+    setCount(x);
+    if (x > 0) {
       setLogout(true);
+    } else {
+      setLogout(false);
     }
   }, []);
 
@@ -39,13 +54,35 @@ function Signin() {
         if (res.data.code === 201) {
           alert("Too many attempts");
           setLogout(true);
-          console.log(res.data.time);
-          localStorage.setItem("TIME", res.data.time);
+          console.log(res.data);
+
+          const year = res.data.current_year;
+          localStorage.setItem("year", year);
+          const month = res.data.current_month;
+          localStorage.setItem("month", month);
+          const date2 = res.data.current_date;
+          localStorage.setItem("date2", date2);
+          const minute = res.data.current_minute;
+          localStorage.setItem("minute", minute);
+          const second = res.data.current_second;
+          localStorage.setItem("second", second);
+          const hour = res.data.current_hour;
+          localStorage.setItem("hour", hour);
+          console.log(
+            year,
+            month,
+            date2,
+            minute,
+            second,
+            hour,
+            "when settign to localstoragee"
+          );
         }
         if (res.data.code === 200) {
           // move to home
           navigate("/home");
           localStorage.setItem("TOKEN", res.data.token);
+          localStorage.setItem("TOKEN", -1);
           localStorage.setItem("EMAIL", res.data.email);
         }
       })
@@ -57,7 +94,7 @@ function Signin() {
   return (
     <>
       {logout ? (
-        <h4>Limit exceded</h4>
+        <h4>You can login after hours {count}</h4>
       ) : (
         <div>
           <h1 className="center"> SIGNIN </h1>
--- clint/src/components/Signup.jsx
+++ clint/src/components/Signup.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import moment from "moment";
+import date from "date-and-time";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -18,6 +19,7 @@ function Signup() {
         console.log(res.data);
         if (res.data.code === 200) {
           alert("Signup success.");
+          localStorage.removeItem("timeout");
           navigate("/signin");
         } else {
           alert("Error.");
@@ -27,6 +29,25 @@ function Signup() {
         console.log(err);
       });
   };
+
+  const now = new Date();
+  // date.format(now, 'YYYY/MM/DD HH:mm:ss')
+  console.log(now, "ssss");
+  now.setHours(now.getHours() + 24);
+  console.log(now, "xxxx");
+
+  // Creating object of given date and time
+  // const date1 = new Date(2022, 2, 12);
+  const date1 = new Date(2022, 11, 8, 15, 15, 23);
+  date.format(date1, "YYYY/MM/DD HH:mm:ss");
+  console.log(date1);
+
+  // Subtracting the both dates
+  // by using date.subtract() method
+  const value = date.subtract(now, date1);
+
+  // Display the result
+  console.log("total days between them " + value.toHours());
 
   return (
     <>
